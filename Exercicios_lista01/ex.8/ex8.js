2f854ca78a104383db9cbcf26e1b0d18fb7450e9
manuela92 <manuela92araujo@gmail.com>
2024-10-29 22:16:56 -0300

    Add files via upload
    Exercícios 8 e 9, lista 01

---
+++ Exercicios_lista01/ex.8/ex8.js
@@ -0,0 +1,5 @@
+salarioh = parseFloat(prompt("Proletarido, quanto você ganha por hora? "));
+hora = parseFloat(prompt("E quantas horas você trabalha por mês? "));
+salariom = salarioh*hora
+
+console.log("Divo, seu salário de milhões é: R$ ", salariom)
---
+++ Exercicios_lista01/ex.9/ex9.js
@@ -0,0 +1,4 @@
+tempf = parseFloat(prompt("Tá calor né? Em farenheit, quantos graus? "));
+tempc = 5*(tempf-32)/9
+
+console.log("Ah sim, em Celcius está ", tempc, "graus")
